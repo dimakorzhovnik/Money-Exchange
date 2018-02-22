022cb471208ca21e5dc2f4e686411ff8b3615439
dimakorzhovnik <korzhovnik.dima@gmail.com>
2018-02-22 06:03:50 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    let result = {};
 
-    if (currency > 10000){
+module.exports = function makeExchange(currency) {
+    var result = {};
+
+    if (currency > 10000) return{
         error: "You are rich, my friend! We don't have so much coins for exchange"
-    } 
+    } ;
 
     while(currency > 0){
         if (currency / 50 >= 1){ //H
-            
-                }
-
-        else if (currency / 25 >= 1){ //Q
-                    
-                }
+            result.H = Math.floor(currency / 50);
+            currency = currency % 50;
+        }
+        else if (currency / 25 >= 1) { //Q
+            result.Q = Math.floor(currency/25);
+            currency = currency % 25;         
+         }
                 
-        else if (currency / 15 >= 1){ //D
-                    
-                }
+        else if (currency / 10 >= 1){ //D
+            result.D = Math.floor(currency/10);
+            currency -= result.D * 10;; 
+         }
 
-        else if (currency / 10 >= 10){ //N
-                    
-                }
-
+        else if (currency / 5 >= 1){ //N
+            result.N = Math.floor(currency/5);
+            currency = currency % 5;   
+        }
         else {  //P
-               result.P = Math.fround(currency / 1);
-               currency = currency % 1;     
-                }
-
-
+               result.P = Math.floor(currency / 1);
+               currency = currency % 1;
+        }
     }
     return result;
 }
